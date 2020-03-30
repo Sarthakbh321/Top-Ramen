1a18114d0a74e9cb7aafea05f5fc4dd156de8972
Sarthak <sarthakbh321@gmail.com>
2020-03-30 16:45:37 +0530

    implemented a search function

--- js/app.js
+++ js/app.js
@@ -15,30 +15,38 @@ $(document).ready(function() {
             data.forEach(function(item) {
                 $(".list").append(`
                 <div class="list-item">
-                <div class="row">
-                  <div class="col-sm-4">
-                    <img src="img/ramen1.jpg" alt="ramen-image" class="list-image">
-                  </div>
-                  <div class="col-sm-8">
-                    <p class="list-content brand">` + item["Brand"] + `</p>
-                    <p class="list-content variety"><span class="item-head variety">` + item["Variety"] + `</span> </p>
-                    <p class="list-content style"><span class="item-head style">` + item["Style"] + `</span> </p>
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <img src="img/ramen1.jpg" alt="ramen-image" class="list-image">
+                        </div>
+                    <div class="col-sm-8">
+                        <p class="list-content brand">` + item["Brand"] + `</p>
+                        <p class="list-content variety"><span class="item-head variety">` + item["Variety"] + `</span> </p>
+                        <p class="list-content style"><span class="item-head style">` + item["Style"] + `</span> </p>
     
-                    <div class="row country-ranking">
-                      <div class="col-sm-4">
-                        <p class="country-head">Country:</p>
-                        <p class="list-content country"><span class="item-head country">` + item["Country"] + `</span> </p>
-                      </div>
-                      <div class="col-sm-4">
-                        <p class="top-ten-head">Ranking: </p>
-                        <p class="list-content top-ten"><span class="item-head top-ten">` + item["Top Ten"] + `</span>
-                      </div>
+                        <div class="row country-ranking">
+                            <div class="col-sm-4">
+                                <p class="country-head">Country:</p>
+                                <p class="list-content country"><span class="item-head country">` + item["Country"] + `</span> </p>
+                        </div>
+                        <div class="col-sm-4">
+                            <p class="top-ten-head">Ranking: </p>
+                            <p class="list-content top-ten"><span class="item-head top-ten">` + item["Top Ten"] + `</span>
+                        </div>
                     </div>
                   </div>
                 </div>
-              </div>
                 `)
             });
         }
+    })
+
+
+    $(".searchbar").on("keyup", function() {
+        var text = $(this).val().toLowerCase();
+
+        $(".list >").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(text) > -1)
+        })
     })
 })
